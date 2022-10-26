f0d078a693406082d1b8b3c702c74497011c2b6b
tabeenfatima <tabeen.it@gmail.com>
2022-10-27 00:22:06 +0530

    first commit

---
+++ SecondProgram.java
@@ -0,0 +1,11 @@
+public class SecondProgram {
+public static void main(String[] arge) {
+	int n=4;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.print(i);
+		}
+		System.out.println();
+	}
+}
+}
